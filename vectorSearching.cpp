5ff1a3637a69f7ab105d7f2a572f722a4d481ce2
Mxtiv <cielenmateusz01@gmail.com>
2024-02-08 17:19:24 +0100

    Search in vector - working

--- vectorSearching.cpp
+++ vectorSearching.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main()
 {
     int val;
-    vector<int> numbers{7,121,1,9};
+    vector<int> numbers{7, 121, 1, 9};
     cin >> val;
-    auto it = find(numbers.begin(),numbers.end(),val);
-    if(it == numbers.end())
-    cout << it - numbers.begin();
+    auto it = find(numbers.begin(), numbers.end(), val);
+    if (it == numbers.end())
+        cout << "not found";
+    else
+        cout << "found at index: " << it - numbers.begin();
 }
