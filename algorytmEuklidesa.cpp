993094660646355740296566ef7b468c5d9960b7
Matthew <cielenmateusz01@gmail.com>
2024-02-14 13:12:22 +0100

    old file added

---
+++ algorytmEuklidesa.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Nwd dwóch liczb"<<endl;
+    cin>>a;
+    cin>>b;
+    while(a!=b){
+    if(a!=b){
+        if(a>b){
+            a = a-b;
+        }else if(a<b){
+            b = b-a;
+        }
+    }}
+    cout<<"nwd to: "<<a;
+}
