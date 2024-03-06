c94fc3edab5a26c2d832222f646c94804e9d1b45
Mxtiv <cielenmateusz01@gmail.com>
2024-03-06 10:44:34 +0100

    for and forEach difference

--- Loops/forEach2dimentions.cpp
+++ Loops/forEach2dimentions.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
-
 int main()
 {
     int arr[4][3]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    int min = arr[0][0];
-
     for (auto &rows : arr)
     {
         for (auto &elem : rows)
         {
-           if(min < elem)
-            min = elem;
+            std::cout << elem << ", ";
         }
     }
-    std::cout << min;
+    std::cout << '\n';
+    int min = arr[0][0];
+    for (auto &rows : arr)
+    {
+        for (auto &elem : rows)
+        {
+            if (min > elem)
+                min = elem;
+        }
+    }
+    int max = arr[0][0];
+    for (auto &rows : arr)
+    {
+        for (auto &elem : rows)
+        {
+            if (max < elem)
+                max = elem;
+        }
+    }
+    std::cout << "min: " << min << '\n';
+    std::cout << "max: " << max << '\n';
 }
